ed308ba6c738a851781a363fddec9403ea5c10a0
chugu <chugunov.alexey@gmail.com>
2024-02-03 18:13:15 +0300

    shooting aliens

--- settings.py
+++ settings.py
@@ -9,8 +9,8 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Bullet settings
-        self.bullet_speed = 2.0
-        self.bullet_width = 3
+        self.bullet_speed = 5
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
